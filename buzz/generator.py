779d6780cd3f808709ba31cba2c1d1fdbac7f9d2
AleksanderAdler <sdepals@gmail.com>
2020-10-25 20:39:05 +0300

    test

--- buzz/generator.py
+++ buzz/generator.py
@@ -17,6 +17,7 @@ def generate_buzz():
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
         sample(verbs), buzz_terms[1]])
+    print(buzz[150])
     return phrase.title()
 
 if __name__ == "__main__":
