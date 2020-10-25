77677d22b6d9c064a8e150d4a1b95bdb3b255ff0
AleksanderAdler <sdepals@gmail.com>
2020-10-25 21:57:32 +0300

    test

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,7 +1,7 @@
 import random
 
 buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
+    'continuous deployment', 'continuous improvement', 'devops', 'developer')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
@@ -17,7 +17,6 @@ def generate_buzz():
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
         sample(verbs), buzz_terms[1]])
-    print(buzz[150])
     return phrase.title()
 
 if __name__ == "__main__":
