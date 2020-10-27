cbffc496f94daee8c7971e099f4e4aeaad62e22e
AleksanderAdler <sdepals@gmail.com>
2020-10-27 17:23:05 +0300

    test

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker login -e $sdepals@gmail.com -u $barrac -p $Barin2451!
+docker login -e sdepals@gmail.com -u barrac -p Barin2451!
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $barrac/cicd-buzz:$tagname .
-docker push $barrac/cicd-buzz
+docker build -f Dockerfile -t barrac/cicd-buzz:tagname
+docker push barrac/cicd-buzz
